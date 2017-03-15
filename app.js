3344a632b609f8f052d7aec4a85752f5cf1d52e2
hiteshchitalia <hiteshc1@icloud.com>
2017-03-15 19:00:51 -0400

    Add flash message for feedback submit

--- app.js
+++ app.js
@@ -4,63 +4,61 @@ var bodyParser = require('body-parser'),
   uuid = require('node-uuid'),
   request = require('request'),
   express = require('express'),
-  session = require('express-session'),
   cookieParser = require('cookie-parser'),
-  flash = require('connect-flash'),
+  session = require('express-session'),
   app = express()
 
+var sessionStore = new session.MemoryStore()
+
 // Configure AWS
-AWS.config.update( {
-  region: "us-east-1",
-  endpoint: "https://dynamodb.us-east-1.amazonaws.com"
+AWS.config.update({
+  region: 'us-east-1',
+  endpoint: 'https://dynamodb.us-east-1.amazonaws.com'
 })
-var docClient = new AWS.DynamoDB.DocumentClient();
+var docClient = new AWS.DynamoDB.DocumentClient()
 
 // APP CONFIG
 app.set('view engine', 'ejs')
 app.set('port', process.env.PORT || 3000)
 app.set('host', process.env.HOST || 'localhost')
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.use(cookieParser('secret'));
-
+app.use(cookieParser('my Little secret'))
 app.use(session({
-  secret: "Mary had a little Harry",
-  resave: false,
-  saveUninitialized: false
-}));
+  secret: 'Mary had a little Harry',
+  store: sessionStore,
+  resave: true,
+  saveUninitialized: true,
+  cookie: {maxAge: 60000}
+}))
 
-app.use(flash());
-
-app.use(function(req, res, next) {
-   res.locals.success = req.flash('success');
-   res.locals.error = req.flash('error');
-   next();
-});
-
-
-// ROUTES
-app.get('/', function (req, res) {
-  res.redirect(303, '/home')
+app.use(function (req, res, next) {
+  res.locals.message = req.session.msg
+  delete req.session.msg
+  next()
 })
 
 // INDEX ROUTE
+app.get('/', function (req, res) {
+  res.render('home')
+})
+
 app.get('/home', function (req, res) {
   res.render('home')
 })
 
-// COLOR PICKER GAME
+// COLOR PICKER GAME ROUTE
 app.get('/colorpicker', function (req, res) {
   res.render('colorPicker')
 })
 
-// TO DO LIST SAMPLE
+// TO DO LIST SAMPLE ROUTE
 app.get('/todolist', function (req, res) {
   res.render('toDoList')
 })
 
-// MOVIE SEARCH
+// MOVIE SEARCH ROUTE
 app.get('/moviesearch', function (req, res) {
   res.render('movieSearch')
 })
@@ -70,11 +68,11 @@ app.get('/movieresult/:id', function (req, res) {
   var url = 'http://www.omdbapi.com/?type=movie&i=' + id
   request(url, function (error, response, body) {
     if (id !== '' && !error && response.statusCode === 200) {
-        var resp = JSON.parse(body)
-        res.render('movieDetails', {movie: resp})
-      } else {
-        res.redirect(303, '/moviesearch')
-      }
+      var resp = JSON.parse(body)
+      res.render('movieDetails', {movie: resp})
+    } else {
+      res.redirect(303, '/moviesearch')
+    }
   })
 })
 
@@ -83,34 +81,41 @@ app.get('/movieresult', function (req, res) {
   var url = 'http://www.omdbapi.com/?type=movie&s=' + st
   request(url, function (error, response, body) {
     if (st !== '' && !error && response.statusCode === 200) {
-        var resp = JSON.parse(body)
-        res.render('movieResults', {data: resp, query: st})
-      } else {
-            // Add flash message and send back to movie search
-        res.redirect(303, '/moviesearch')
-      }
+      var resp = JSON.parse(body)
+      res.render('movieResults', {data: resp, query: st})
+    } else {
+      req.session.msg = {type: 'error', msg: 'Error connecting to API.  Please resubmit request.'}
+      res.redirect(303, '/moviesearch')
+    }
   })
 })
 
+// FEEDBACK ROUTE
 app.post('/feedback', function (req, res) {
-    var id = uuid.v1()
-    var fb = {id: id, feedbackText: req.body.fb};
-    var params = {
-      TableName: "Portfolio",
-      Item: fb
+  var id = uuid.v1()
+  var ts = new Date().getTime().toString()
+  var insertItem = {
+      id: id,
+      feedbackText: req.body.fb,
+      Timestamp: ts
     }
-    docClient.put(params, function(err, newlyCreated) {
-        if (err) {
-          req.flash("error", "Error connecting to database.  Feedback not saved.")
+  var params = {
+      TableName: 'Portfolio',
+      Item: insertItem
+    }
+  docClient.put(params, function (err, newlyCreated) {
+      if (err) {
+          req.session.msg = {type: 'error', msg: 'Error connecting database.  Feedback not saved.'}
         } else {
-          //newlyCreated.save();
-          req.flash("success", "Thanks for your feedback!")
+          req.session.msg = {type: 'success', msg: 'Thanks for your feedback!'}
         }
+      res.redirect(303, '/home')
     })
-    res.redirect(303, '/home');
 })
 
 app.listen(app.get('port'), app.get('host'), function () {
   console.log('App started on http://' + app.get('host') + ':' +
-    app.get('port') + '; press Ctrl-C to terminate.')
+   app.get('port') + '; press Ctrl-C to terminate.')
 })
+
+// module.exports = app;
